a8900cd7518f5c97abce1def67603cf0ea7a8363
German <germandanielacosta@outlook.com>
2025-12-08 15:32:05 -0500

    Changes

--- Rest API/src/main/java/com/employee/controller/EmployeeController.java
+++ Rest API/src/main/java/com/employee/controller/EmployeeController.java
@@ -3,12 +3,15 @@ package com.employee.controller;
 import com.employee.dto.EmployeeRequestDTO;
 import com.employee.dto.EmployeeResponseDTO;
 import com.employee.service.EmployeeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/employees")
+@Tag(name = "Employees", description = "Endpoints para gestionar empleados")
 public class EmployeeController {
     private final EmployeeService employeeService;
 
@@ -19,6 +22,7 @@ public class EmployeeController {
 
 
     @GetMapping
+    @Operation(summary = "Crear un nuevo empleado", description = "Procesa y guarda un nuevo empleado en la base de datos. Valida que sea mayor de edad.")
     public ResponseEntity<EmployeeResponseDTO> receiveEmployee(@Valid @RequestBody EmployeeRequestDTO request) {
 
 
--- Rest API/src/main/java/com/employee/exception/GlobalExceptionHandler.java
+++ Rest API/src/main/java/com/employee/exception/GlobalExceptionHandler.java
@@ -3,14 +3,14 @@ package com.employee.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
