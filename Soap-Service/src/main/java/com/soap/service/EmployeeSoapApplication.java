cc2778e72d1bd95b19980d32ad4cfc3ce62df553
German <germandanielacosta@outlook.com>
2025-12-08 22:16:03 -0500

    Renaming the application

--- Soap-Service/src/main/java/com/soap/service/EmployeeSoapApplication.java
+++ Soap-Service/src/main/java/com/soap/service/EmployeeSoapApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SoapServiceApplication {
+public class EmployeeSoapApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SoapServiceApplication.class, args);
+		SpringApplication.run(EmployeeSoapApplication.class, args);
 	}
 
 }
--- Soap-Service/src/main/java/com/soap/service/ServletInitializer.java
+++ Soap-Service/src/main/java/com/soap/service/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SoapServiceApplication.class);
+		return application.sources(EmployeeSoapApplication.class);
 	}
 
 }
